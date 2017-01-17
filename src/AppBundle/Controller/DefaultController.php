6a7f7bcd7df04d8204ab5461112647ce461fc568
Maxime <maxime.code@bbox.fr>
2017-01-17 17:01:20 +0100

    courrier envoyé

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -18,4 +19,20 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/mail")
+     */
+    public function mailAction(Request $request)
+    {
+        // Create a message
+        $message = \Swift_Message::newInstance('Wonderful Subject')
+          ->setFrom(array('maxime.code@bbox.fr' => 'Maxime Code'))
+          ->setTo(array('maxime.public@bbox.fr'))
+          ->setBody('Here is the message itself');
+
+        // Send the message
+        $this->get('mailer')->send($message);   
+
+        return new Response('<html><body>Mail envoyé !</body></html>');
+    } 
 }
